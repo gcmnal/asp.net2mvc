d359a8ca2110e1aaf55f682f32d9d123fadd0d3f
ali göçmen <gcmnl@icloud.com>
2018-09-18 11:47:47 +0300

    Add project files.

---
+++ asp.net2mvc/Controllers/SharedController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp.net2mvc.Controllers
+{
+    public class SharedController : Controller
+    {
+        public IActionResult _Layout()
+        {
+            return View();
+        }
+    }
+}
